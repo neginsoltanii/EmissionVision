f228a44a70a3c399ef2ee75dd01411bbe022abc2
Nicklas Bourelius <145897592+nick7ass@users.noreply.github.com>
2024-05-17 16:40:49 +0200

    Inputs with Pun2 not working yet (WiP)

--- Assets/OurScripts/GlobeControllerScript.cs
+++ Assets/OurScripts/GlobeControllerScript.cs
@@ -3,103 +3,116 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using WPM;
+using Photon.Pun;
 
-public class GlobeControllerScript : MonoBehaviour
+public class GlobeControllerScript : MonoBehaviourPun // Inherit from MonoBehaviourPun
 {
-    
     public DataManager dataManager;
-    
-
-    public float rotateSpeed = 10f; // degrees per second
+    public float rotateSpeed; // degrees per second
     float step;
     public ColorizeCountriesScript colorizeScript;
-    public int selectedYear = 1991;
-    public GameObject globe;
+    public int selectedYear;
 
     // Start is called before the first frame update
     void Start()
     {
         colorizeScript.map.ToggleCountrySurface("Brazil", true, Color.blue);
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Test code from g
-       /* if (OVRInput.GetUp(OVRInput.RawButton.Y) || OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+        if (!photonView.IsMine) return; // If it's not the local player's object, don't execute the input code
+
+        HandleInput();
+        HandleRotation();
+    }
+
+    void HandleInput()
+    {
+        // Handle color change input
+        if (OVRInput.GetUp(OVRInput.RawButton.Y) || OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
         {
-            colorizeScript.ColorizeCountries(selectedYear);
-            Debug.Log("Hello From inside the if conidition");
-            //colorizeScript.map.ToggleCountrySurface("Brazil", true, Color.green);
-        }*/
+            photonView.RPC("ColorizeCountriesRPC", RpcTarget.All, selectedYear); // Call RPC to colorize countries for all players
+            Debug.Log("Hello from inside the if condition");
+        }
 
         // Handle year selection logic
         if (OVRInput.GetUp(OVRInput.RawButton.A))
         {
-            //selectedYear = Mathf.Max(selectedYear - 1, 1991); // Adjust year bounds as needed
-            //Debug.Log("Selected Year:" + selectedYear);
-            //colorizeScript.ColorizeCountries(selectedYear);
-            colorizeScript.map.ToggleCountrySurface("Iran", true, Color.blue);
-
+            SelectPreviousYear();
         }
         else if (OVRInput.GetUp(OVRInput.RawButton.B))
         {
-            //selectedYear = Mathf.Min(selectedYear + 1, 2021); // Adjust year bounds as needed
-            //Debug.Log("Selected Year:" + selectedYear);
-            //colorizeScript.ColorizeCountries(selectedYear);
-            colorizeScript.map.ToggleCountrySurface("Iran", true, Color.red);
+            SelectNextYear();
         }
-        //End test
-        //// Check if there is a connected gamepad
-        //if (Gamepad.current != null)
-        //{
-        //    Vector2 rotationInput = Gamepad.current.rightStick.ReadValue();
-        //  //  Vector2 rotationInput = new Vector2(Input.GetAxis("RightStickHorizontal"), Input.GetAxis("RightStickVertical"));
-        //
-        //
-        //    // Rotate around the Y axis
-        //    transform.Rotate(0, rotationInput.x * rotateSpeed * Time.deltaTime, 0);
-        //}
+    }
 
-
-        // Documentation from: https://developer.oculus.com/documentation/unity/unity-tutorial-basic-controller-input/
-
-
-        /*
-        //if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
-        if (OVRInput.GetUp(OVRInput.RawButton.A))
-        {
-            //// Testing send data to Data UI Manager
-            //DataFormatWorld data = new DataFormatWorld();
-            //data.countryName = "Sweden";
-            //data.co2emissions = 2300;
-
-            // Read a value from the CSV file
-            DataFormatWorld data = dataManager.GetTestValueFromYear();
-            dataManager.SetDataToUI(data);
-        }*/
-
-
-
-
-        /// Rotate with the thumbsticks
-
-        // Calculate a proportion in the rotation (degrees per second)
+    void HandleRotation()
+    {
         step = rotateSpeed * Time.deltaTime;
+
         if (OVRInput.Get(OVRInput.RawButton.RThumbstickLeft))
         {
-            globe.transform.Rotate(0, step, 0);
-            Debug.Log("Right Thumbstick detected - Left");
+            RotateLeft();
         }
 
         if (OVRInput.Get(OVRInput.RawButton.RThumbstickRight))
         {
-            globe.transform.Rotate(0, -step, 0);
-            Debug.Log("Right Thumbstick detected - Right");
+            RotateRight();
         }
-
     }
-        
+
+    void SelectPreviousYear()
+    {
+        selectedYear = Mathf.Max(selectedYear - 1, 1991); // Adjust year bounds as needed
+        photonView.RPC("UpdateSelectedYearRPC", RpcTarget.All, selectedYear); // Call RPC to update selected year for all players
+    }
+
+    void SelectNextYear()
+    {
+        selectedYear = Mathf.Min(selectedYear + 1, 2018); // Adjust year bounds as needed
+        photonView.RPC("UpdateSelectedYearRPC", RpcTarget.All, selectedYear); // Call RPC to update selected year for all players
+    }
+
+    [PunRPC]
+    void UpdateSelectedYearRPC(int year)
+    {
+        selectedYear = year;
+        Debug.Log("Selected Year: " + selectedYear);
+        colorizeScript.ColorizeCountries(selectedYear);
+    }
+
+    [PunRPC]
+    void ColorizeCountriesRPC(int year)
+    {
+        Debug.Log("Colorizing countries for year: " + year);
+        colorizeScript.ColorizeCountries(year);
+    }
+
+    void RotateLeft()
+    {
+        transform.Rotate(0, step, 0);
+        photonView.RPC("RotateLeftRPC", RpcTarget.Others); // Send RPC to other clients
+        Debug.Log("Right Thumbstick detected - Left");
+    }
+
+    void RotateRight()
+    {
+        transform.Rotate(0, -step, 0);
+        photonView.RPC("RotateRightRPC", RpcTarget.Others); // Send RPC to other clients
+        Debug.Log("Right Thumbstick detected - Right");
+    }
+
+    [PunRPC]
+    void RotateLeftRPC()
+    {
+        transform.Rotate(0, step, 0);
+    }
+
+    [PunRPC]
+    void RotateRightRPC()
+    {
+        transform.Rotate(0, -step, 0);
+    }
 }
